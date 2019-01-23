2fb9c26a53c460dff35c9290f6bbde785eb47473
blithe-database-2858 <amavelamala@gmail.com>
2019-01-23 03:11:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,5 +29,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 funkyFunction();
-var theFunk = funkyFunction;
+var theFunk = function {
+  return funkyFunction();
+}
 theFunk();
